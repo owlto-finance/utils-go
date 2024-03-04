df91df5c70a12aaf2173881fe481f1c5dc68dcba
wankai <wankaizhang@gmail.com>
2024-03-04 15:54:11 +0800

    init

--- main.go
+++ main.go
@@ -1,43 +1,5 @@
 package main
 
-// import (
-// 	"fmt"
-
-// 	"github.com/owlto-finance/utils-go/config"
-// )
-
-// type Tel struct {
-// 	Num []int `mapstructure:"num"` // mapstructure tag should match the field name
-// }
-
-// type Conf struct {
-// 	DbBackend       string `mapstructure:"db_backend"`
-// 	DbDsn           string `mapstructure:"db_dsn"`
-// 	KmsUrl          string `mapstructure:"kms_url"`
-// 	KmsApiKey       string `mapstructure:"kms_api_key"`
-// 	ApiServerListen string `mapstructure:"api_server_listen"`
-// 	Telemetry       Tel    `mapstructure:"telemetry"`
-// 	IsTestnet       bool   `mapstructure:"is_testnet"`
-// }
-
-// func main() {
-// 	conf := Conf{
-// 		DbBackend:       "mysql",
-// 		DbDsn:           "usbase",
-// 		KmsUrl:          "ht.example.com",
-// 		KmsApiKey:       "api_key",
-// 		ApiServerListen: ":9090",
-// 		Telemetry:       Tel{Num: []int{3}},
-// 		IsTestnet:       true,
-// 	}
-
-// 	err := config.WriteConfigFile("/home/lzj/utils-go/a/b/c.toml", &conf)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	fmt.Println(conf)
-// }
-
 func main() {
 
 }

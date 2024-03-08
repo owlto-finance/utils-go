719dd1bbc323dcd185532a51953804d0e6b85717
wankai <wankaizhang@gmail.com>
2024-03-08 14:03:30 +0800

    add code

---
+++ loader/token_info.go
@@ -0,0 +1,90 @@
+package loader
+
+import (
+	"database/sql"
+	"log"
+	"sync"
+
+	"github.com/owlto-finance/utils-go/alert"
+)
+
+type TokenInfo struct {
+	TokenName    string
+	ChainName    string
+	TokenAddress string
+	Decimals     int32
+}
+
+type TokenInfoManager struct {
+	chainNameTokenAddrs map[string]map[string]*TokenInfo
+
+	db      *sql.DB
+	alerter alert.Alerter
+	mutex   *sync.RWMutex
+}
+
+func NewTokenInfoManager(db *sql.DB, alerter alert.Alerter) *TokenInfoManager {
+	return &TokenInfoManager{
+		chainNameTokenAddrs: make(map[string]map[string]*TokenInfo),
+
+		db:      db,
+		alerter: alerter,
+		mutex:   &sync.RWMutex{},
+	}
+}
+
+func (mgr *TokenInfoManager) GetByChainNameTokenAddr(chainName string, tokenAddr string) (*TokenInfo, bool) {
+	mgr.mutex.RLock()
+	defer mgr.mutex.RUnlock()
+	tokenAddrs, ok := mgr.chainNameTokenAddrs[chainName]
+	if ok {
+		token, ok := tokenAddrs[tokenAddr]
+		return token, ok
+	}
+	return nil, false
+}
+
+func (mgr *TokenInfoManager) LoadAllToken() {
+	// Query the database to select only id and name fields
+	rows, err := mgr.db.Query("SELECT token_name, chain_name, token_address, decimals FROM t_token_info")
+
+	if err != nil || rows == nil {
+		mgr.alerter.AlertText("select t_token_info error", err)
+		return
+	}
+
+	defer rows.Close()
+
+	chainNameTokenAddrs := make(map[string]map[string]*TokenInfo)
+	counter := 0
+
+	// Iterate over the result set
+	for rows.Next() {
+		var token TokenInfo
+
+		if err := rows.Scan(&token.TokenName, &token.ChainName, &token.TokenAddress, &token.Decimals); err != nil {
+			mgr.alerter.AlertText("scan t_token_info row error", err)
+		} else {
+			tokenAddrs, ok := chainNameTokenAddrs[token.ChainName]
+			if !ok {
+				tokenAddrs = make(map[string]*TokenInfo)
+				chainNameTokenAddrs[token.ChainName] = tokenAddrs
+			}
+			tokenAddrs[token.TokenAddress] = &token
+
+			counter++
+		}
+	}
+
+	// Check for errors from iterating over rows
+	if err := rows.Err(); err != nil {
+		mgr.alerter.AlertText("get next t_token_info row error", err)
+		return
+	}
+
+	mgr.mutex.Lock()
+	mgr.chainNameTokenAddrs = chainNameTokenAddrs
+	mgr.mutex.Unlock()
+	log.Println("load all token info: ", counter)
+
+}

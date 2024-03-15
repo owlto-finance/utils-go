a7439c7d105859f507c6187f688d041b641cb539
owlto <owlto.finance@gmail.com>
2024-03-15 15:14:58 +0800

    add lazy alerter

--- alert/common.go
+++ alert/common.go
@@ -1,5 +1,60 @@
 package alert
 
+import (
+	"log"
+	"sync"
+	"time"
+)
+
 type Alerter interface {
 	AlertText(msg string, err error)
+	AlertTextLazy(msg string, err error)
+	AlertTextLazyGroup(group string, msg string, err error)
+}
+
+type CommonAlerter struct {
+	lazyInterval int64
+	lazyReset    int64
+	lazyTimer    map[string]int64
+	mutex        *sync.RWMutex
+}
+
+func NewCommonAlerter(lazyInterval int64, lazyReset int64) *CommonAlerter {
+	return &CommonAlerter{
+		lazyInterval: lazyInterval,
+		lazyReset:    lazyReset,
+		lazyTimer:    make(map[string]int64),
+		mutex:        &sync.RWMutex{},
+	}
+}
+
+func (ca *CommonAlerter) AlertText(msg string, err error) {
+	log.Printf("%s : %v", msg, err)
+}
+
+func (ca *CommonAlerter) AlertTextLazyGroup(group string, msg string, err error) {
+	ca.DoAlertTextLazy(ca, group, msg, err)
+}
+
+func (ca *CommonAlerter) AlertTextLazy(msg string, err error) {
+	ca.DoAlertTextLazy(ca, "", msg, err)
+}
+
+func (ca *CommonAlerter) DoAlertTextLazy(target Alerter, group string, msg string, err error) {
+	ca.mutex.Lock()
+	defer ca.mutex.Unlock()
+	timer, ok := ca.lazyTimer[group]
+	now := time.Now().Unix()
+	if !ok {
+		ca.lazyTimer[group] = now
+	} else {
+		if now-timer >= ca.lazyReset {
+			ca.lazyTimer[group] = now
+			return
+		}
+		if now-timer >= ca.lazyInterval {
+			target.AlertText(msg, err)
+			ca.lazyTimer[group] = now
+		}
+	}
 }
--- alert/lark.go
+++ alert/lark.go
@@ -12,22 +12,32 @@ import (
 // alerter.AlertText("fuck", errors.New("shit"))
 
 type LarkAlerter struct {
+	*CommonAlerter
 	webhook string
 }
 
 func NewLarkAlerter(webhook string) *LarkAlerter {
 	return &LarkAlerter{
-		webhook: strings.TrimSpace(webhook),
+		webhook:       strings.TrimSpace(webhook),
+		CommonAlerter: NewCommonAlerter(180, 900),
 	}
 }
 
-func (alerter *LarkAlerter) AlertText(msg string, err error) {
+func (la *LarkAlerter) AlertTextLazyGroup(group string, msg string, err error) {
+	la.DoAlertTextLazy(la, group, msg, err)
+}
+
+func (la *LarkAlerter) AlertTextLazy(msg string, err error) {
+	la.DoAlertTextLazy(la, "", msg, err)
+}
+
+func (la *LarkAlerter) AlertText(msg string, err error) {
 	data := map[string]interface{}{
 		"msg_type": "text",
 		"content": map[string]interface{}{
 			"text": fmt.Sprintf("%s : %v", msg, err),
 		},
 	}
-	network.Request(alerter.webhook, data, nil)
+	network.Request(la.webhook, data, nil)
 	log.Println(msg, ":", err)
 }

0f6a065e7a5ce320352b31f6c1fd786f174551da
wankai <wankaizhang@gmail.com>
2024-04-18 15:44:28 +0800

    src-add-new-param

--- loader/src_tx.go
+++ loader/src_tx.go
@@ -22,6 +22,7 @@ type SrcTx struct {
 	SrcTokenDecimal int32
 	IsCctp          int32
 	SrcNonce        int32
+  Thirdparty_channel int32
 }
 
 type SrcTxManager struct {
@@ -53,11 +54,11 @@ func (mgr *SrcTxManager) Save(tx *SrcTx) error {
 	tx.TargetAddress.String = strings.TrimSpace(tx.TargetAddress.String)
 	tx.SrcTokenName.String = strings.TrimSpace(tx.SrcTokenName.String)
 
-	query := `INSERT IGNORE INTO t_src_transaction (chainid, tx_hash, sender, receiver, target_address, token, value, dst_chainid, is_testnet, tx_timestamp, src_token_name, src_token_decimal, is_cctp, src_nonce)
-              VALUES (?, ?, ?, ?, ?, ?, ? , ?, ?, ?, ?, ?, ?, ?)`
+	query := `INSERT IGNORE INTO t_src_transaction (chainid, tx_hash, sender, receiver, target_address, token, value, dst_chainid, is_testnet, tx_timestamp, src_token_name, src_token_decimal, is_cctp, src_nonce, thirdparty_channel)
+              VALUES (?, ?, ?, ?, ?, ?, ? , ?, ?, ?, ?, ?, ?, ?, ?)`
 
 	// Execute the SQL statement with tx data
-	_, err := mgr.db.Exec(query, tx.ChainId, tx.TxHash, tx.Sender, tx.Receiver, tx.TargetAddress, tx.Token, tx.Value, tx.DstChainid, tx.IsTestnet, tx.TxTimestamp, tx.SrcTokenName, tx.SrcTokenDecimal, tx.IsCctp, tx.SrcNonce)
+	_, err := mgr.db.Exec(query, tx.ChainId, tx.TxHash, tx.Sender, tx.Receiver, tx.TargetAddress, tx.Token, tx.Value, tx.DstChainid, tx.IsTestnet, tx.TxTimestamp, tx.SrcTokenName, tx.SrcTokenDecimal, tx.IsCctp, tx.SrcNonce, tx.Thirdparty_channel)
 	if err != nil {
 		mgr.alerter.AlertText("failed to insert src transaction", err)
 		return err

75dcbffa5ef8d494329a1310232d1e4f40ec8c42
wankai <wankaizhang88@gmail.com>
2024-06-21 12:38:09 +0800

    Add WithLogIDCtx function in util/generate.go
    A new function named `WithLogIDCtx` has been added to the `util/generate.go` file. This function generates a log ID and attaches it to the given context, allowing easy context-related logging. It uses the previously existing `GenerateLogID` function to generate the log ID.

--- util/generate.go
+++ util/generate.go
@@ -1,15 +1,22 @@
 package util
 
 import (
+	"context"
 	"fmt"
 	"math/rand"
 	"time"
 )
 
+var loc, _ = time.LoadLocation("Asia/Shanghai")
+
 func GenerateLogID() string {
-	now := time.Now()
+	now := time.Now().In(loc)
 	random := rand.New(rand.NewSource(now.UnixNano()))
 	timestamp := now.Format("20060102150405")
 	randomPart := fmt.Sprintf("%018d", random.Int63())
 	return fmt.Sprintf("%s%s", timestamp, randomPart)
+}
+
+func WithLogIDCtx(ctx context.Context, logId string) context.Context {
+	return context.WithValue(ctx, "logId", logId)
 }

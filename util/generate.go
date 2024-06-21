4f5078919baa05a05a6ea4db0f04864c37463efe
wankai <wankaizhang88@gmail.com>
2024-06-21 15:06:42 +0800

    Remove redundant SetLogId function
    The SetLogId function in the util/generate.go file has been removed as it was redundant. The functionality is already provided by the WithLogIDCtx function, rendering SetLogId unnecessary and causing possible confusion.

--- util/generate.go
+++ util/generate.go
@@ -14,6 +14,9 @@ func GenerateLogID() string {
 	random := rand.New(rand.NewSource(now.UnixNano()))
 	timestamp := now.Format("20060102150405")
 	randomPart := fmt.Sprintf("%018d", random.Int63())
+	if len(randomPart) > 18 {
+		randomPart = randomPart[:18]
+	}
 	return fmt.Sprintf("%s%s", timestamp, randomPart)
 }
 
